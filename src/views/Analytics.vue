6bfd515a40f776390d798b8376cfdd6504faec61
rajadavid423 <rajadavid423@gmail.com>
2023-07-19 21:29:03 +0530

    chart added in analytics page

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -3,6 +3,7 @@
   <div :style="{ 'margin-left': sidebarWidth }">
     <div class="about">
       <h1>Analytics Page</h1>
+      <DoughnutChart :chartData="testData" />
     </div>
   </div>
 </template>
@@ -11,13 +12,27 @@
 import { collapsed, toggleSidebar } from '@/components/sidebar/state'
 import Sidebar from '@/components/sidebar/Sidebar.vue'
 import { sidebarWidth } from '@/components/sidebar/state'
+import { DoughnutChart } from 'vue-chart-3'
+import { Chart, registerables } from "chart.js"
+
+Chart.register(...registerables)
 
 export default {
   components: {
-    Sidebar
+    Sidebar,
+    DoughnutChart
   },
   setup() {
-    return { collapsed, toggleSidebar, sidebarWidth }
+    const testData = {
+      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+      datasets: [
+        {
+          data: [30, 40, 60, 70, 5],
+          backgroundColor: ['#77ff82', '#dc2433', '#0a4f02', '#48a3ee', '#A5C8ED'],
+        }
+      ]
+    }
+    return { collapsed, toggleSidebar, sidebarWidth, testData }
   }
 }
 </script>
