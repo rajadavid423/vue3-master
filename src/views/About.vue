1cc343ba7a0e7f2f99eef171b42ed16861a468e8
rajadavid423 <rajadavid423@gmail.com>
2023-07-19 21:33:46 +0530

    about page content sample added

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,58 @@
   <div :style="{ 'margin-left': sidebarWidth }">
     <div class="about">
       <h1>About Page</h1>
+      <div class="container-fluid">
+        <b-skeleton-wrapper>
+          <b-card>
+            1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra nunc sapien,
+            non rhoncus elit tincidunt vitae. Vestibulum maximus, ligula eu feugiat molestie,
+            massa diam imperdiet odio, vitae viverra ligula est id nisi. Aliquam ut molestie est.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Phasellus at consequat dui. Aenean tristique sagittis quam,
+            sit amet sollicitudin neque sodales in.
+          </b-card>
+          <br>
+          <br>
+          <b-card>
+            2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra nunc sapien,
+            non rhoncus elit tincidunt vitae. Vestibulum maximus, ligula eu feugiat molestie,
+            massa diam imperdiet odio, vitae viverra ligula est id nisi. Aliquam ut molestie est.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Phasellus at consequat dui. Aenean tristique sagittis quam,
+            sit amet sollicitudin neque sodales in.
+          </b-card>
+          <br>
+          <br>
+          <b-card>
+            3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra nunc sapien,
+            non rhoncus elit tincidunt vitae. Vestibulum maximus, ligula eu feugiat molestie,
+            massa diam imperdiet odio, vitae viverra ligula est id nisi. Aliquam ut molestie est.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Phasellus at consequat dui. Aenean tristique sagittis quam,
+            sit amet sollicitudin neque sodales in.
+          </b-card>
+          <br>
+          <br>
+          <b-card>
+            4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra nunc sapien,
+            non rhoncus elit tincidunt vitae. Vestibulum maximus, ligula eu feugiat molestie,
+            massa diam imperdiet odio, vitae viverra ligula est id nisi. Aliquam ut molestie est.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Phasellus at consequat dui. Aenean tristique sagittis quam,
+            sit amet sollicitudin neque sodales in.
+          </b-card>
+          <br>
+          <br>
+          <b-card>
+            5. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra nunc sapien,
+            non rhoncus elit tincidunt vitae. Vestibulum maximus, ligula eu feugiat molestie,
+            massa diam imperdiet odio, vitae viverra ligula est id nisi. Aliquam ut molestie est.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Phasellus at consequat dui. Aenean tristique sagittis quam,
+            sit amet sollicitudin neque sodales in.
+          </b-card>
+        </b-skeleton-wrapper>
+      </div>
     </div>
   </div>
 </template>
